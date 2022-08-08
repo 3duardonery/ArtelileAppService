8fb545822f89ac1fb00145bea6cb76fdd1215ca7
Eduardo Nery <edu.nery.cordeiro@gmail.com>
2022-08-08 11:51:21 -0300

    :deploy: Deploy new version

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -106,7 +106,6 @@ export class AuthController {
   async createHash(password: string): Promise<string> {
     const saltOrRounds = 10;
     const hash = await bcrypt.hash(password, saltOrRounds);
-
     return hash;
   }
 }
